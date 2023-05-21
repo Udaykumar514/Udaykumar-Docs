f93ef03ff895c9e7770b8f33076f944ee930fa5c
Udaykumar514 <133563620+Udaykumar514@users.noreply.github.com>
2023-05-21 11:01:10 +0530

    Add files via upload

---
+++ AbstractAndInterfacesConceptnJava/src/com/java/examples/corejava/MethodOverridingExample.java
@@ -0,0 +1,26 @@
+package com.java.examples.corejava;
+
+//MethodOverriding Example  //Method Overriding is Two different classes
+
+class A{
+	void add(int a, int b) {
+		System.out.println("Im add method in A and result is ::" +(a+b));
+	}
+}
+
+class B extends A{
+	void add(int p, int q) {
+		System.out.println("Im add method in B and result is ::" +(p+q));
+	}
+}
+
+public class MethodOverridingExample {
+
+	public static void main(String[] args) {
+		
+		B ob = new B();
+		ob.add(5, 5);
+		ob.add(2, 3);
+		
+	}
+}
